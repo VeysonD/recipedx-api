05b6d9b951a9c3825f54650f0f18121210b2185b
Rick Gallegos <duulketariakan@yahoo.com>
2018-02-11 22:40:51 -0800

    Add JWT authentication

--- server/route-handler.js
+++ server/route-handler.js
@@ -2,13 +2,29 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const jwt = require('express-jwt');
+const jwks = require('jwks-rsa');
 
 app = express();
+
+const jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: "https://fdash4.auth0.com/.well-known/jwks.json"
+  }),
+  audience: 'http://localhost:4201',
+  issuer: 'https://fdash4.auth0.com',
+  algorithms: ['RS256']
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(cors());
+// Uncomment the jwtCheck middleware to enable authentication
+// app.use(jwtCheck);
 
 app.get('/api/recipes', (req, res, next) => {
   console.log('Testing get');
