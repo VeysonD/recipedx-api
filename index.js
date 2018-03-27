e32c19dd9a6d957c4bb8e1315edb713edaa5965c
Rick Gallegos <duulketariakan@yahoo.com>
2018-03-26 20:26:54 -0700

    Add dotenv package for deployment

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const dotenv = require('dotenv');
 const server = require('./server/route-handler.js');
 
+dotenv.config();
+
 const SERVER = process.env.SERVER || 'http://127.0.0.1';
-const PORT = process.env.PORT || 4201
+const PORT = process.env.PORT || 4201;
 
 server.listen(PORT);
 console.log(`Server is listening at ${SERVER}:${PORT}`);
